0402800a8f66f92722662ae9fd04c1bc8216904c
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-04-16 16:53:12 +0200

    Cleanup, refactor into seperate functions, simplified post logic

--- appengine/app.js
+++ appengine/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const PubSub = require('@google-cloud/pubsub');
+const express = require("express");
+const PubSub = require("@google-cloud/pubsub");
 
 // Create a new PubSub client using the GOOGLE_CLOUD_PROJECT
 // environment variable. This is automatically set to the correct
@@ -9,45 +9,65 @@ const pubsubClient = new PubSub({
 });
 
 const app = express();
-
 // For any request to /public/{some_topic}, push a simple
 // PubSub message to that topic.
-app.get('/publish/:topic', async (req, res) => {
-  const topic = req.params['topic'];
-  setTimeout(async function() { 
-    try {
-      await pubsubClient.topic(topic)
-          .publisher()
-          .publish(Buffer.from('test'));
-    } catch (e) {
-      console.log(e)
-    }
-   }, 30000);
+
+app.get("/publish/public-game", async (req, res) => {
   try {
-    await pubsubClient.topic(topic)
+    var x = 0;
+    var intervalID = setInterval(async () => {
+      var currentdate = new Date();
+      // Your logic here
+      await pubsubClient
+        .topic("post-question")
         .publisher()
-        .publish(Buffer.from('test'));
-    res.status(200).send('Published to ' + topic).end();
+        .publish(Buffer.from("000000"));
+
+        // after 12 seconds, evaluate the last question
+        setTimeout(async () => {
+          // Your logic here
+          await pubsubClient
+            .topic("evaluate-question")
+            .publisher()
+            .publish(Buffer.from("000000"));
+        }, 12000);
+
+      if (++x === 10) {
+        clearInterval(intervalID);
+      }
+    }, 30000);
+    res
+      .status(200)
+      .send("Published to public game channel" )
+      .end();
   } catch (e) {
-    res.status(500).send('' + e).end();
+    res
+      .status(500)
+      .send("" + e)
+      .end();
   }
 });
 
-
 // Index page, just to make it easy to see if the app is working.
-app.get('/time', (req, res) => {
+app.get("/time", (req, res) => {
   var currentTime = new Date();
-  res.status(200).send(currentTime.getTime()).end();
+  res
+    .status(200)
+    .send(currentTime.getTime())
+    .end();
 });
 
 // Index page, just to make it easy to see if the app is working.
-app.get('/', (req, res) => {
-    res.status(200).send('[functions-cron]: Hello, world!').end();
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("[functions-cron]: Hello, world!")
+    .end();
 });
 
 // Start the server
 const PORT = process.env.PORT || 6060;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  console.log("Press Ctrl+C to quit.");
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,6 +19,68 @@ import { Result } from "range-parser";
 admin.initializeApp(functions.config().firebase);
 export const db = admin.firestore();
 
+exports.new_round = functions.pubsub
+  .topic("new-round")
+  .onPublish(async message => {
+    return true;
+  });
+exports.evaluate_question = functions.pubsub
+  .topic("evaluate-question")
+  .onPublish(async message => {
+    var channel = "000000"
+    if (message.data) {
+      channel = Buffer.from(message.data, "base64").toString();
+    }
+    const channelRef = db.collection("channels").doc(channel);
+
+    const round = await channelRef.get();
+
+
+
+    // deactivate the current round
+    channelRef.update({
+      active: false,
+      result: round.data()["correctAnswerIndex"]
+    })
+
+    return true;
+  });
+exports.post_question = functions.pubsub
+  .topic("post-question")
+  .onPublish(async message => {
+    var channel = "000000"
+    if (message.data) {
+      channel = Buffer.from(message.data, "base64").toString();
+    }
+    const channelRef = db.collection("channels").doc(channel);
+
+    // get question
+    var question = await getFallbackQuestion();
+    console.log(question);
+    const answers = [question.correct_answer, ...question.incorrect_answers];
+
+    // Shuffle the answers array
+    for (let i = 0; i < answers.length; i++) {
+      const randomChoiceIndex = Math.floor(Math.random() * answers.length);
+      // place our random choice in the spot by swapping
+      [answers[i], answers[randomChoiceIndex]] = [
+        answers[randomChoiceIndex],
+        answers[i]
+      ];
+    }
+    const correctAnswerIndex = answers.indexOf(question.correct_answer);
+    //const roundRef = channelRef.collection("rounds").doc();
+
+    channelRef.set({
+      question: question.question,
+      answers: answers,
+      correctAnswerIndex: correctAnswerIndex, 
+      active: true,
+      result: -1
+    });
+    return true;
+  });
+
 exports.minutely_job = functions.pubsub
   .topic("minutely-tick")
   .onPublish(async message => {
@@ -28,139 +90,149 @@ exports.minutely_job = functions.pubsub
     }
 
     // get question
-    var question = await getFallbackQuestion(); 
+    var question = await getFallbackQuestion();
     console.log(question);
     const answers = [question.correct_answer, ...question.incorrect_answers];
 
     // Shuffle the answers array
     for (let i = 0; i < answers.length; i++) {
-      const randomChoiceIndex = Math.floor(Math.random()* answers.length);
+      const randomChoiceIndex = Math.floor(Math.random() * answers.length);
       // place our random choice in the spot by swapping
-      [answers[i], answers[randomChoiceIndex]] = [answers[randomChoiceIndex], answers[i]];
+      [answers[i], answers[randomChoiceIndex]] = [
+        answers[randomChoiceIndex],
+        answers[i]
+      ];
     }
-    const correctAnswerIndex = answers.indexOf(question.correct_answer); 
-    //const roundRef = channelRef.collection("rounds").doc(); 
-    
+    const correctAnswerIndex = answers.indexOf(question.correct_answer);
+    //const roundRef = channelRef.collection("rounds").doc();
+
     const channelRef = db.collection("channels").doc("en");
-    
+
     channelRef.set({
-      question: question.question, 
+      question: question.question,
       answers: answers,
-      correctAnswerIndex: -1, 
-    })
+      correctAnswerIndex: -1
+    });
 
     // TODO set correct answer index some time in the future
-    
+
     setTimeout(() => {
       channelRef.update({
-        correctAnswerIndex, 
-      })
-      evaluateGuesses(channelRef)
+        correctAnswerIndex
+      });
     }, 15000);
-    
-
-
-    // TODO generate translations and question collection supertype.
-
-    // TODO increase 
-
-
-    // TODO write the old questions metadata
-    // TODO kick quaraneened players
 
     return true;
   });
 
-  async function evaluateRound(channelRef){
-    let roundSnapshot = await channelRef.get();
-    // console.log(roundSnapshot.data());
-    
-    const upvotes =  await channelRef.collection("upvotes").get(); 
-    const downvotes =  await channelRef.collection("downvotes").get(); 
-    const issues =  await channelRef.collection("issues").get(); 
-    const guesses =  await channelRef.collection("guesses").get(); 
+async function evaluateRound(channelRef) {
+  let roundSnapshot = await channelRef.get();
+  // console.log(roundSnapshot.data());
 
-    // console.log("Documents length")
-    // console.log(upvotes.docs.length)
-    // console.log(downvotes.docs.length)
-    if(upvotes.docs.length > downvotes.docs.length ){
-      // check if the question allready exists
-      // TODO check if this works
-      const questionExists = await db.collection("questions_test").where('question', '==', roundSnapshot.data()["question"]).get();
-      // console.log("question snap")
-      // console.log(questionExists.docs)
-      if(questionExists.docs.length > 0){
-        // TODO test
-        questionExists.docs.forEach((item) => {
-          item.ref.update({
-            upvotesCount: item["upvotesCount"] + upvotes.docs.length, 
-            downvotesCount: item["downvotesCount"] + downvotes.docs.length, 
-            issuesCount: item["issuesCount"] + issues.docs.length, 
-            guessesCount: item["guessesCount"] + guesses.docs.length, 
-            correctGuessesCount: item["correctGuessesCount"] + guesses.docs.filter((doc) => {doc["index"] == roundSnapshot.data()["correctAnswerIndex"]}).length,
-          });
-        })
-      }
-      else{
-        const questionRef = db.collection("questions_test").doc(); 
-        questionRef.set({
-            category: roundSnapshot.data()["category"],
-            difficulty: roundSnapshot.data()["difficulty"],
-            question: roundSnapshot.data()["question"],
-            correctAnswer: roundSnapshot.data()["answers"][roundSnapshot.data()["correctAnswerIndex"]],
-            answers: roundSnapshot.data()["answers"].filter((item) => item != roundSnapshot.data()["answers"][roundSnapshot.data()["correctAnswerIndex"]]),
-            upvotesCount: upvotes.docs.length, 
-            downvotesCount: downvotes.docs.length, 
-            issuesCount: issues.docs.length, 
-            guessesCount: guesses.docs.length, 
-            correctGuessesCount: guesses.docs.filter((doc) => {doc["index"] == roundSnapshot.data()["correctAnswerIndex"]}).length,
-        })
-      }
+  const upvotes = await channelRef.collection("upvotes").get();
+  const downvotes = await channelRef.collection("downvotes").get();
+  const issues = await channelRef.collection("issues").get();
+  const guesses = await channelRef.collection("guesses").get();
+
+  // console.log("Documents length")
+  // console.log(upvotes.docs.length)
+  // console.log(downvotes.docs.length)
+  if (upvotes.docs.length > downvotes.docs.length) {
+    // check if the question allready exists
+    // TODO check if this works
+    const questionExists = await db
+      .collection("questions_test")
+      .where("question", "==", roundSnapshot.data()["question"])
+      .get();
+    // console.log("question snap")
+    // console.log(questionExists.docs)
+    if (questionExists.docs.length > 0) {
+      // TODO test
+      questionExists.docs.forEach(item => {
+        item.ref.update({
+          upvotesCount: item["upvotesCount"] + upvotes.docs.length,
+          downvotesCount: item["downvotesCount"] + downvotes.docs.length,
+          issuesCount: item["issuesCount"] + issues.docs.length,
+          guessesCount: item["guessesCount"] + guesses.docs.length,
+          correctGuessesCount:
+            item["correctGuessesCount"] +
+            guesses.docs.filter(doc => {
+              doc["index"] == roundSnapshot.data()["correctAnswerIndex"];
+            }).length
+        });
+      });
+    } else {
+      const questionRef = db.collection("questions_test").doc();
+      questionRef.set({
+        category: roundSnapshot.data()["category"],
+        difficulty: roundSnapshot.data()["difficulty"],
+        question: roundSnapshot.data()["question"],
+        correctAnswer: roundSnapshot.data()["answers"][
+          roundSnapshot.data()["correctAnswerIndex"]
+        ],
+        answers: roundSnapshot
+          .data()
+          ["answers"].filter(
+            item =>
+              item !=
+              roundSnapshot.data()["answers"][
+                roundSnapshot.data()["correctAnswerIndex"]
+              ]
+          ),
+        upvotesCount: upvotes.docs.length,
+        downvotesCount: downvotes.docs.length,
+        issuesCount: issues.docs.length,
+        guessesCount: guesses.docs.length,
+        correctGuessesCount: guesses.docs.filter(doc => {
+          doc["index"] == roundSnapshot.data()["correctAnswerIndex"];
+        }).length
+      });
     }
-
-    deleteCollection(upvotes);
-    deleteCollection(downvotes);
-    deleteCollection(issues);
-    deleteCollection(guesses);
   }
-  async function evaluateGuesses(channelRef){
-    const guesses =  await channelRef.collection("guesses").get(); 
 
-    console.log(guesses.docs)
-    guesses.docs.forEach((item) => console.log(item.data().index))
-    channelRef.update({results: {
-      // upvotesCoung: upvotes.docs.length, 
-      // downvotesCount: downvotes.docs.length, 
-      // issuesCount: issues.docs.length, 
-      guessesCount: guesses.docs.length, 
+  deleteCollection(upvotes);
+  deleteCollection(downvotes);
+  deleteCollection(issues);
+  deleteCollection(guesses);
+}
+async function evaluateGuesses(channelRef) {
+  const guesses = await channelRef.collection("guesses").get();
+
+  console.log(guesses.docs);
+  guesses.docs.forEach(item => console.log(item.data().index));
+  channelRef.update({
+    results: {
+      // upvotesCoung: upvotes.docs.length,
+      // downvotesCount: downvotes.docs.length,
+      // issuesCount: issues.docs.length,
+      guessesCount: guesses.docs.length,
       guesses: {
-        0: guesses.docs.filter((doc) => doc.data().index == 0).length,
-        1: guesses.docs.filter((doc) => doc.data().index == 1).length,
-        2: guesses.docs.filter((doc) => doc.data().index == 2).length,
-        3: guesses.docs.filter((doc) => doc.data().index == 3).length,
-      }
-    }})
-  }
-  function deleteCollection(snapshot){
-    snapshot.forEach((doc) => {
-      doc.ref.delete();
-    });
-  }
-  async function getFallbackQuestion(){
-    var question; 
-    while(!question){
-      try{
-        var questionRequest = await request(
-          "https://opentdb.com/api.php?amount=1&category=9&type=multiple"
-        );
-        console.log(questionRequest);
-        questionRequest = questionRequest.replace(/&quot;/g, '\\"');
-        questionRequest = questionRequest.replace(/&#039;/g, '\\`');
-        const questionResponse = JSON.parse(unescape(questionRequest));
-        question = questionResponse.results[0]
-      }
-      catch{
+        0: guesses.docs.filter(doc => doc.data().index == 0).length,
+        1: guesses.docs.filter(doc => doc.data().index == 1).length,
+        2: guesses.docs.filter(doc => doc.data().index == 2).length,
+        3: guesses.docs.filter(doc => doc.data().index == 3).length
       }
     }
-    return question;
+  });
+}
+function deleteCollection(snapshot) {
+  snapshot.forEach(doc => {
+    doc.ref.delete();
+  });
+}
+async function getFallbackQuestion() {
+  var question;
+  while (!question) {
+    try {
+      var questionRequest = await request(
+        "https://opentdb.com/api.php?amount=1&category=9&type=multiple&encode=url3986"
+      );
+      console.log(questionRequest);
+      questionRequest = questionRequest.replace(/&quot;/g, '\\"');
+      questionRequest = questionRequest.replace(/&#039;/g, "\\`");
+      const questionResponse = JSON.parse(unescape(questionRequest));
+      question = questionResponse.results[0];
+    } catch {}
   }
+  return question;
+}
